2a8a05c5508e167f38600e42e3fbb94c6da92d33
eooce <142894633+eooce@users.noreply.github.com>
2024-06-16 03:37:04 +0800

    VPS一键四合一脚本(reality+vmess-argo+hysteria2+tuic5)

--- sing-box.sh
+++ sing-box.sh
@@ -359,12 +359,6 @@ echo ""
 
 # 配置nginx订阅配置
 add_nginx_conf() {
-sed -i '/location \/ {/ {
-n
-a\
-\n        location \/sub {\n            alias \/etc\/sing-box\/sub.txt;\n            default_type "text/plain; charset=utf-8";\n        }
-}' /etc/nginx/nginx.conf
-
 new_config="server {
     listen $nginx_port;
 

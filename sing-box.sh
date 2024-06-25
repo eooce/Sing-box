451719ce2becf54a1acf37a46b42d606c084202e
eooce <142894633+eooce@users.noreply.github.com>
2024-06-25 10:08:03 +0800

    VPS一键四合一脚本(reality+vmess-argo+hysteria2+tuic5)

--- sing-box.sh
+++ sing-box.sh
@@ -1258,5 +1258,5 @@ while true; do
         0) exit 0 ;;
         *) red "无效的选项，请输入 0 到 8" ;; 
    esac
-   read -n 1 -s -r -p $'\033[1;91m按任意键继续...\033[0m'
+   read -n 1 -s -r -p yellow "按任意键继续..."
 done

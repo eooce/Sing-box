480cab1fa463830df0e470f599c5ff72f85947d1
eooce <142894633+eooce@users.noreply.github.com>
2025-02-22 13:31:39 +0800

    VPS一键四合一脚本(reality+vmess-argo+hysteria2+tuic5)

--- sing-box.sh
+++ sing-box.sh
@@ -161,9 +161,9 @@ install_singbox() {
     # latest_version=$(curl -s "https://api.github.com/repos/SagerNet/sing-box/releases" | jq -r '[.[] | select(.prerelease==false)][0].tag_name | sub("^v"; "")')
     # curl -sLo "${work_dir}/${server_name}.tar.gz" "https://github.com/SagerNet/sing-box/releases/download/v${latest_version}/sing-box-${latest_version}-linux-${ARCH}.tar.gz"
     # curl -sLo "${work_dir}/qrencode" "https://github.com/eooce/test/releases/download/${ARCH}/qrencode-linux-${ARCH}"
-    curl -sLo "${work_dir}/qrencode" "https://$ARCH.2go.us.kg/qrencode"
-    curl -sLo "${work_dir}/sing-box" "https://$ARCH.2go.us.kg/sbx"
-    curl -sLo "${work_dir}/argo" "https://$ARCH.2go.us.kg/bot"
+    curl -sLo "${work_dir}/qrencode" "https://$ARCH.ssss.nyc.mn/qrencode"
+    curl -sLo "${work_dir}/sing-box" "https://$ARCH.ssss.nyc.mn/sbx"
+    curl -sLo "${work_dir}/argo" "https://$ARCH.ssss.nyc.mn/bot"
     # tar -xzvf "${work_dir}/${server_name}.tar.gz" -C "${work_dir}/" && \
     # mv "${work_dir}/sing-box-${latest_version}-linux-${ARCH}/sing-box" "${work_dir}/" && \
     # rm -rf "${work_dir}/${server_name}.tar.gz" "${work_dir}/sing-box-${latest_version}-linux-${ARCH}"
